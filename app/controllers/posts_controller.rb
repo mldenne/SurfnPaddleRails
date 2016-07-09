22a96131c609281bfb057f8178e1a190516c2573
Marie Denney <mldenne@gmail.com>
2016-07-09 09:56:51 -0400

    Added changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
 
   def index
+    @title = "Surf and Paddle Co."
     @post = Post.all.sort_by { |post| post.created_at }.reverse.first
     @other_post = Post.all.reject { |post| post == @post }
   end
 
   def show
     @post = Post.all.find { |post| post.id == params[:id].to_i }
+    @other_post = Post.all.reject { |post| post == @post }
   end
 
 end
