004a54b9c401209ddb5ae4320edb06f19113f17c
Marie Denney <mldenne@gmail.com>
2016-07-09 13:18:47 -0400

    Added changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
 
   def index
-    @title = "Surf and Paddle Co."
+    @title = "Surf & Paddle Co."
+    @subtitle = "A Fourth Cohort Indianapolis IronYard Project"
+    @blogtitle = "Surf & Paddle Co. Blog"
     @post = Post.all.sort_by { |post| post.created_at }.reverse.first
     @other_post = Post.all.reject { |post| post == @post }
   end
 
   def show
+    @title = "Surf & Paddle Co."
+    @subtitle = "A Fourth Cohort Indianapolis IronYard Project"
+    @blogtitle = "Surf & Paddle Co. Blog"
     @post = Post.all.find { |post| post.id == params[:id].to_i }
     @other_post = Post.all.reject { |post| post == @post }
   end
