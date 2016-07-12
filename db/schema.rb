a22d36c1b44a4a7c9439a9612eccc394916bbf44
Marie Denney <mldenne@gmail.com>
2016-07-12 17:00:30 -0400

    Created Author model, created association between author and post, updated erb with author logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712181026) do
+ActiveRecord::Schema.define(version: 20160712195155) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.string   "selfie"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,9 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save a post without a title" do
+    post = Post.new(:body => "this is a test")
+    assert_not post.save, "Saved the post without a title"
+  end
+
 end
